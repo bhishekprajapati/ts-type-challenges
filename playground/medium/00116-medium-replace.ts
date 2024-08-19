22ca92f491f86ca77e61177c79554c0363c97b51
Abhishek Prajapati <bhishekprajapati@gmail.com>
2024-08-19 09:12:04 +0530

    complete: 00116-medium-replace

--- playground/medium/00116-medium-replace.ts
+++ playground/medium/00116-medium-replace.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> = From extends "" ? S : S  extends `${infer Prefix}${From}${infer Suffix}` ? `${Prefix}${To}${Suffix}` : S;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
