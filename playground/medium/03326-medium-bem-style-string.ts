cddabeeb44e24d9aa08d07758b5b53340ac6d39a
Abhishek Prajapati <bhishekprajapati@gmail.com>
2024-08-31 13:11:38 +0530

    complete: 03326-medium-bem-style-string

--- playground/medium/03326-medium-bem-style-string.ts
+++ playground/medium/03326-medium-bem-style-string.ts
@@ -16,15 +16,15 @@
 
 /* _____________ Your Code Here _____________ */
 
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type BEM<B extends string, E extends string[], M extends string[]> = E['length'] extends 0 ? `${B}--${M[number]}` : M[number] extends never ? `${B}__${E[number]}` : `${B}__${E[number]}--${M[number]}`
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<BEM<'btn', ['price'], []>, 'btn__price'>>,
-  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success' >>,
-  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large' >>,
+  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success'>>,
+  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large'>>,
 ]
 
 /* _____________ Further Steps _____________ */
